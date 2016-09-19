ed44239912eb5cc5030b568192327328e031bed5
vagrant <vagrant@homestead>
2016-09-19 22:06:25 +0000

    First commit

---
+++ app/Events/DashboardEvent.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class DashboardEvent implements ShouldBroadcast {
+
+  /**
+   * Get the channels the event should broadcast on.
+   */
+  public function broadcastOn() {
+    return new PrivateChannel('dashboard');
+  }
+}
